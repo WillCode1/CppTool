3e896f7e25fe3172204876f50c70a50b1a562ac1
Will <2623556566@qq.com>
2021-07-16 00:03:12 +0800

    add gtest

---
+++ Code/HelloGtest/MyEKF.h
@@ -0,0 +1,13 @@
+#pragma once
+
+
+class MyEKF
+{
+public:
+	int Add()
+	{
+	}
+	int Sub()
+	{
+	}
+};
---
+++ Code/HelloGtest/TestEKF.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "gtest/gtest.h"
+#include <iostream>
+
+
+class TestEKF: public testing::Test
+{
+protected:
+	// 整组测试用例调用前后执行
+	static void SetUpTestCase()
+	{
+		std::cout << "SetUpTestCase" << std::endl;
+	}
+	static void TearDownTestCase()
+	{
+		std::cout << "TearDownTestCase" << std::endl;
+	}
+
+	// 每个测试用例调用前后执行
+	virtual void SetUp()
+	{
+		std::cout << "SetUp" << std::endl;
+	}
+	virtual void TearDown()
+	{
+		std::cout << "TearDown" << std::endl;
+	}
+
+private:
+
+};
