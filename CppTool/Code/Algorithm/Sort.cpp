6c08065dff7197bdda88e60d79b9c168d043c84b
Will <2623556566@qq.com>
2021-10-18 22:04:25 +0800

    sort

--- CppTool/Code/Algorithm/Sort.cpp
+++ CppTool/Code/Algorithm/Sort.cpp
@@ -1,5 +1,70 @@
 #include "Sort.h"
 
+
+void Sort::bubbleSort(vector<int>& array)
+{
+	for (int j = array.size() - 1; j >= 0; j--)
+	{
+		bool find = false;
+		for (int i = 0; i < j; i++)
+			if (array[i] > array[i + 1])
+			{
+				swap(array[i], array[i + 1]);
+				find = true;
+			}
+		if (!find)
+			return;
+	}
+}
+
+//简单选择
+void Sort::selectSort(vector<int>& nums)
+{
+	int n = nums.size();
+	for (int i = 0; i < n - 1; i++) {
+		int min = i;
+		for (int j = i + 1; j < n; j++) {
+			if (nums[j] < nums[min])
+				min = j;
+
+		}
+		if (min != i)
+			swap(nums[i], nums[min]);
+	}
+}
+
+//插入
+void Sort::insertSort(vector<int>& nums)
+{
+	for (int i = 1; i < nums.size(); i++)
+	{
+		int temp = nums[i];
+		int j = i - 1;
+		while (j >= 0 && nums[j] > temp)
+		{
+			nums[j + 1] = nums[j];
+			j--;
+		}
+		nums[j + 1] = temp;
+	}
+}
+
+//希尔
+void Sort::shellSort(vector<int>& nums, int n)
+{
+	for (int dk = n / 2; dk >= 1; dk = dk / 2) {
+		for (int i = dk; i < n; ++i) {
+			if (nums[i] < nums[i - dk]) {
+				int tmp = nums[i], j;
+				for (j = i - dk; j >= 0 && tmp < nums[j]; j -= dk) {
+					nums[j + dk] = nums[j];
+				}
+				nums[j + dk] = tmp;
+			}
+		}
+	}
+}
+
 // https://www.cnblogs.com/sky20080101/articles/8693343.html
 /*
 （二）挖坑法:
@@ -12,7 +77,7 @@
 5.把轴放在low所指的位置
 6.分别对low所指的位置的左边和右边进行上述的递归
 */
-void Sort::quick(vector<int>& array, int left, int right)
+void Sort::quickSort(vector<int>& array, int left, int right)
 {
 	if (left < right)
 	{
@@ -34,8 +99,8 @@ void Sort::quick(vector<int>& array, int left, int right)
 		array[low] = pivot;
 		//printArray(array);
 
-		quick(array, left, low - 1);
-		quick(array, low + 1, right);
+		quickSort(array, left, low - 1);
+		quickSort(array, low + 1, right);
 	}
 }
 
@@ -62,10 +127,66 @@ void Sort::quick(vector<int>& array, int left, int right)
 //	}
 //}
 
-void Sort::bubble(vector<int>& array)
+// 归并
+void Sort::merge(vector<int>& arr, int left, int mid, int right)
 {
-	for (int j = array.size() - 1; j >= 0; j--)
-		for (int i = 0; i < j; i++)
-			if (array[i] > array[i + 1])
-				swap(array[i], array[i + 1]);
+	vector<int> L_arr(arr.begin() + left, arr.begin() + mid);
+	vector<int> R_arr(arr.begin() + mid, arr.begin() + right + 1);
+
+	int i = 0, j = 0, k = left;
+	while (i < L_arr.size() && j < R_arr.size()) {
+		if (L_arr[i] < R_arr[j])
+			arr[k++] = L_arr[i++];
+		else
+			arr[k++] = R_arr[j++];
+	}
+
+	while (i < L_arr.size())
+		arr[k++] = L_arr[i++];
+	while (j < R_arr.size())
+		arr[k++] = R_arr[j++];
 }
+
+void Sort::mergeSort(vector<int>& arr, int left, int right)
+{
+	if (left == right)
+		return;
+	else {
+		int mid = (left + right) / 2;
+		mergeSort(arr, left, mid);
+		mergeSort(arr, mid + 1, right);
+		merge(arr, left, mid + 1, right);
+	}
+}
+
+// https://blog.csdn.net/alzzw/article/details/98087519
+// 堆排序
+void Sort::adjust(vector<int>& nums, int len, int index)
+{
+	int left = 2 * index + 1;	// index的左子节点
+	int right = 2 * index + 2;	// index的右子节点
+
+	int maxIdx = index;
+	if (left < len && nums[left] > nums[maxIdx])
+		maxIdx = left;
+	if (right < len && nums[right] > nums[maxIdx])
+		maxIdx = right;
+
+	if (maxIdx != index)
+	{
+		swap(nums[maxIdx], nums[index]);
+		adjust(nums, len, maxIdx);
+	}
+}
+
+void Sort::heapSort(vector<int>& nums, int size)
+{
+	for (int root = size / 2 - 1; root >= 0; root--) {
+		adjust(nums, size, root);
+	}
+	for (int i = size - 1; i >= 1; i--) {
+		swap(nums[0], nums[i]);
+		adjust(nums, i, 0);
+	}
+}
+
