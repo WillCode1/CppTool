45571e1f76b0e9d5c578606a7768c0b27c25f2d4
Will <2623556566@qq.com>
2022-01-09 21:07:46 +0800

    add eigen

--- CppTool/Code/Eigen/Pose.cpp
+++ CppTool/Code/Eigen/Pose.cpp
@@ -32,3 +32,11 @@ void testPose3d()
     cout << pose.matrix() << endl;
     cout << pose.translation() << endl;
 }
+
+Eigen::Quaterniond RollPitchYaw(const double roll, const double pitch, const double yaw)
+{
+    const Eigen::AngleAxisd roll_angle(roll, Eigen::Vector3d::UnitX());
+    const Eigen::AngleAxisd pitch_angle(pitch, Eigen::Vector3d::UnitY());
+    const Eigen::AngleAxisd yaw_angle(yaw, Eigen::Vector3d::UnitZ());
+    return yaw_angle * pitch_angle * roll_angle;
+}
