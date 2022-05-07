0f04902b43c07df60efcfd53bd473d62b79560a3
Will <2623556566@qq.com>
2022-05-07 10:14:26 +0800

    asd

--- Slam/avp_mapping/src/avp_mapping/g2o/edge_se2_xyprior.cc
+++ Slam/avp_mapping/src/avp_mapping/g2o/edge_se2_xyprior.cc
@@ -9,6 +9,10 @@ bool EdgeSE2XYPrior::read(std::istream &is) { return false; }
 
 bool EdgeSE2XYPrior::write(std::ostream &os) const { return false; }
 
+/*
+    _error = vi->estimate().translation() - _measurement;
+    Vector2D error(x,y)对VertexSE2 vi(x,y,theta)求导, 2x3矩阵: 
+ */
 void EdgeSE2XYPrior::linearizeOplus() { _jacobianOplusXi << 1, 0, 0, 0, 1, 0; }
 
 } // end namespace
--- Slam/slambook/math_lib/nonlinearOptimization/g2oCurveFitting.cpp
+++ Slam/slambook/math_lib/nonlinearOptimization/g2oCurveFitting.cpp
@@ -46,7 +46,7 @@ public:
   virtual void computeError() override {
     const CurveFittingVertex *v = static_cast<const CurveFittingVertex *> (_vertices[0]);
     const Eigen::Vector3d abc = v->estimate();
-    _error(0, 0) = _measurement - std::exp(abc(0, 0) * _x * _x + abc(1, 0) * _x + abc(2, 0));   // 观测值在前, -1 x jacobx
+    _error(0, 0) = _measurement - std::exp(abc(0, 0) * _x * _x + abc(1, 0) * _x + abc(2, 0));
   }
 
   // 计算雅可比矩阵
